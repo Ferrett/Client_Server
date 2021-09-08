7c85df8e80ed9477e0c660f1df8910c67d9277ad
Prikhod322 <vovkaprikhod@gmail.com>
2021-09-08 11:10:27 +0300

    InitCommit

---
+++ CLient_serv/CLient_serv/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CLient_serv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
